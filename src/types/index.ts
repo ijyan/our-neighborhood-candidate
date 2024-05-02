1f09c2be1eb0bf18af71025533bb51b1a22ec9cc
ijyan <guuhwi@gmail.com>
2024-05-03 03:26:23 +0900

    Comment: 타입 주석

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,46 +1,39 @@
 /**
- * 기본 타입
- *
- * @param pageNo {number} - 페이지 번호
- * @param numOfRows {number} - 한 페이지 결과 수
+ * 정당코드를 조회하는 타입
  */
-export interface IApiProp {
-  pageNo?: number;
-  numOfRows?: number;
-}
-
-/**
- * 정당코드 조회 타입
- */
-export interface ICommonPartyCode extends IApiProp {
-  sgId: number;
+export interface ICommonPartyCode {
+  pageNo?: number; // 페이지 번호
+  numOfRows?: number; // 한 페이지 결과 수
+  sgId: string; // 선거 ID(예: 20240410)
 }
 
 export interface IPartyCode extends ICommonPartyCode {
-  num: number;
-  jdName: string;
-  pOrder: string;
-  image?: string;
-  color?: string;
+  num: number; // 결과 순서
+  jdName: string; // 정당명
+  pOrder: string; // 순서
+  image?: string; // 정당 이미지
+  color?: string; // 정당 색깔
 }
 
 /**
- * 정당정책정보 조회 타입
+ * 정당정책정보를 조회하는 타입
  */
-export interface ICommonPartyPlcInfoInqire extends IApiProp {
-  sgId: string;
-  partyName: string | undefined;
+export interface ICommonPartyPlcInfoInqire {
+  pageNo?: number; // 페이지 번호
+  numOfRows?: number; // 한 페이지 결과 수
+  sgId: string; // 선거 ID(예: 20240410)
+  partyName: string | undefined; // 정당명(예: 00당)
 }
 
 export interface IPartyPlcInfoInqire {
-  num: string;
-  sgId: string;
-  partyName: string;
-  prmsCnt: string;
-  prmsOrd1: string;
-  prmsRealmName1: string;
-  prmsTitle1: string;
-  prmmCont1: string;
+  num: string; // 결과 순서
+  sgId: string; // 선거 ID(예: 20240410)
+  partyName: string; // 정당명
+  prmsCnt: string; // 공약개수
+  prmsOrd1: string; // 공약순번1
+  prmsRealmName1: string; // 공약분야명1
+  prmsTitle1: string; // 공약제목명1
+  prmmCont1: string; // 공약내용
   prmsOrd2: string;
   prmsRealmName2: string;
   prmsTitle2: string;
@@ -86,50 +79,55 @@ export interface IPartyList {
 }
 
 /**
- * 당선인 조회 타입
+ * 당선인 정보를 조회하는 타입
  */
-export interface ICommonWinnerProps extends IApiProp {
-  sgId: number;
-  sgTypecode: number;
-  sdName?: string;
-  sggName?: string;
+export interface ICommonWinnerProps {
+  pageNo: number; // 페이지 번호
+  numOfRows: number; // 한 페이지 결과 수
+  sgId: number; // 선거 ID (예: 20240410)
+  sgTypecode: number; // 선거종류코드(예: 2)
+  sdName?: string; // 시도명
+  sggName?: string; // 선거구명
 }
 
+/**
+ * 당선인 정보를 조회하는 타입
+ */
 export interface IWinnerInfo extends ICommonWinnerProps {
-  num: string;
+  num: string; // 결과순서
   // sgId: number;
   // sgTypecode: number;
-  huboid: string;
+  huboid: string; // 후보자 ID
   // sggName: string;
   // sdName: string;
-  wiwName: string;
-  giho: number;
-  gihoSangse: string;
-  jdName: string;
-  name: string;
-  hanjaName: string;
-  gender: string;
-  birthday: number;
-  age: number;
-  addr: string;
-  jobId: number;
-  job: string;
-  eduId: number;
-  edu: string;
-  career1: string;
-  career2: string;
-  dugsu: number;
-  dugyul: number;
-  image?: string;
+  wiwName: string; // 구시군명
+  giho: number; // 기호
+  gihoSangse: string; // 기호상세
+  jdName: string; // 정당명
+  name: string; // 한글성명
+  hanjaName: string; // 한자성명
+  gender: string; // 성별
+  birthday: number; // 생년월일
+  age: number; // 연령
+  addr: string; // 주소
+  jobId: number; // 직업ID
+  job: string; // 직업
+  eduId: number; // 학력ID
+  edu: string; // 학력
+  career1: string; // 경력1
+  career2: string; // 경력2
+  dugsu: number; // 득표수
+  dugyul: number; // 득표율
+  image?: string; // 프로필이미지
 }
 
 /**
  * CITY_LIST 타입
  */
 export interface ICityList {
-  id: string;
-  city: string;
-  districts?: string[];
+  id: string; // 지역번호
+  city: string[]; // 광역시도
+  districts: string[]; // 시군구
 }
 
 /**
@@ -145,9 +143,9 @@ export interface ApiResponse<T> {
       items: {
         item: T[];
       };
-      numOfRows: number;
-      pageNo: number;
-      totalCount: number;
+      numOfRows: number; // 목록건수
+      pageNo: number; // 페이지번호
+      totalCount: number; // 총건수
     };
   };
 }
